894682c087b0dd2269f1c1e7d20db1b8ab52bfac
nils-nm <nilsfuruskog@yandex.ru>
2021-10-12 21:14:48 +0300

    commit

--- project/main.py
+++ project/main.py
@@ -1,47 +1,46 @@
-from kivy.app import App
+
+
+from kivymd.app import MDApp
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRaisedButton
 from kivy.uix.togglebutton import ToggleButton
+from kivymd.uix.bottomsheet import MDCustomBottomSheet
+from kivy.factory import Factory
+
 from kivy.lang import Builder
 from kivy.clock import Clock
 import time
-Builder.load_file('main.kv')
+kv = Builder.load_file('main.kv')
 
 
-class TimeWindow(Screen):
-    def spin(self, win):
-        pass
-
+class TimeWindow(MDScreen):
     def time(self, *args):
-        self.ids.time_in.text = str(time.strftime('%H:%M', time.localtime()))
+        MDRaisedButton.text = str(time.strftime('%H:%M', time.localtime()))
 
 
-class SettingsWindow(Screen):
-    def color(self, col):
-        if col == 'r':
-            sm.get_screen('time').ids.time_in.color = (1, 0, 0, 1)
-        elif col == 'g':
-            sm.get_screen('time').ids.time_in.color = (0, 1, 0, 1)
-        elif col == 'b':
-            sm.get_screen('time').ids.time_in.color = (0, 0, 1, 1)
+# class SettingsWindow(Screen):
+#    def color(self, col):
+#        if col == 'r':
+#            sm.get_screen('time').ids.time_in.color = (1, 0, 0, 1)
+#        elif col == 'g':
+#            sm.get_screen('time').ids.time_in.color = (0, 1, 0, 1)
+#        elif col == 'b':
+#            sm.get_screen('time').ids.time_in.color = (0, 0, 1, 1)
 
 
-class WODWindow(Screen):
-    pass
+class MyApp(MDApp):
+    custom_sheet = None
 
-
-sm = ScreenManager()
-sm.add_widget(TimeWindow(name='time'))
-sm.add_widget(SettingsWindow(name='set'))
-sm.add_widget(WODWindow(name='wod'))
-
-
-class MyApp(App):
     def build(self):
 
-        Clock.schedule_interval(sm.get_screen('time').time, 1)
-        return sm
+        Clock.schedule_interval(TimeWindow.time, 1)
+
+        return kv
+
+    def secbs(self):
+        self.custom_sheet = MDCustomBottomSheet(screen=Factory.ContentCustomSheet())
+        self.custom_sheet.open()
 
 
 if __name__ == '__main__':
